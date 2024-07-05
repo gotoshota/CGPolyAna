1695049a4e1998db1bafa059722745eec15c84b5
Shota Goto <49383594+gotoshota@users.noreply.github.com>
2024-07-05 18:29:48 +0900

    fix

--- src/modules/coord_convert.f90
+++ src/modules/coord_convert.f90
@@ -173,8 +173,8 @@ contains
         orthogonal_coords = coords
         ly = box_dim(2, 2) - box_dim(1, 2)
         xy = box_dim(3, 1)
-        orthogonal_coords(1, :) = coords(1, :) + coords(2, :) * xy / sqrt(ly*ly - xy*xy)
-        orthogonal_coords(2, :) = coords(2, :) * ly / sqrt(ly*ly - xy*xy)
+        orthogonal_coords(1, :) = coords(1, :) + coords(2, :) * xy / sqrt(ly*ly + xy*xy)
+        orthogonal_coords(2, :) = coords(2, :) * ly / sqrt(ly*ly + xy*xy)
     end function triclinic_to_orthogonal_real
     function triclinic_to_orthogonal_traj(traj) result(orthogonal_traj)
         implicit none
