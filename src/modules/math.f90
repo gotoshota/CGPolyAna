b144f4f664fc8b1c370178390537c66a17b0473e
shota goto <sg@myMacMini.local>
2024-09-11 04:43:19 +0900

    first commit

--- src/modules/math.f90
+++ src/modules/math.f90
@@ -70,4 +70,48 @@ contains
         c(3) = a(1)*b(2) - a(2)*b(1)
     end function
     ! ===========================================
+
+    ! ===========================================
+    ! topological calculation
+    function gauss_linking_number(curve1, curve2) result(gln)
+        implicit none
+        real(4), intent(in) :: curve1(:, :), curve2(:, :)
+        real(8) :: linking_number
+        real(8) :: r1(3), r2(3), dr1(3), dr2(3), d_cross(3), diff(3)
+        real(8) :: norm_diff
+        real(8) :: eps = 1.0d-8 ! あんまり小さくすると誤差が大きくなる
+        integer :: i, j
+        integer :: n_points
+
+        linking_number = 0.0d0
+        n_points = size(curve1, 2)
+
+        ! Gauss linking number の計算
+        do i = 1, n_points
+            r1 = dble(curve1(:,i))
+            do j = 1, n_points
+                r2 = dble(curve2(:,j))
+
+                ! r1 - r2 の計算
+                diff = r1 - r2
+                norm_diff = sqrt(sum(diff**2)) + eps
+
+                ! dr1 と dr2 (次の点への差分ベクトル)
+                ! dr1 と dr2 の計算（iがn_pointsを超えないようにする）
+                dr1 = dble(curve1(:,mod(i, n_points) + 1)) - r1
+                dr2 = dble(curve2(:,mod(j, n_points) + 1)) - r2
+
+                ! 外積を計算
+                call cross_product(dr1, dr2, d_cross)
+
+                ! Gauss linking number に寄与する部分を加算
+                ! ここでnorm_diff が小さすぎると誤差が大きくなるのでepsを足している
+                linking_number = linking_number + dot_product(diff, d_cross) / (norm_diff**3.0d0)
+            end do
+        end do
+
+        linking_number = linking_number / (4.0d0 * acos(-1.0d0))  ! 4πで割る
+    end function
+    ! ===========================================
+
 end module
